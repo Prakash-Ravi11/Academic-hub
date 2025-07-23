fdc4c26d185b7a765ea949b61b6a3f288f31b48d
R.R.PRAKASH RAVI <185219267+Prakash-Ravi11@users.noreply.github.com>
2025-07-23 20:57:49 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,21 @@
-const CACHE_NAME = 'academic-hub-v1.0';
+const CACHE_NAME = 'academic-hub-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+    '/',
+    '/manifest.json',
+    '/icons/icon-192.png',
+    '/icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
 });
